939b2476a48944290cb507c7925ac02c6a5e2d6d
PietroRantin <rantin.pietro@gmail.com>
2026-04-05 12:12:18 -0300

    Commit Inicial

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-# Teste de versionamento
-print("Just a test")
-print("Just a test")
-print("Just a test")
+import pygame as pg
+
+# ctr + alt + l (formatar pep8)
+
+print('Start Setup')
+pg.init()
+tela = pg.display.set_mode(size=(640, 480))
+print('End Setup')
+
+print('Start Loop')
+while True:
+    # Check for all events
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()  # Close Window
+            quit()  # End Pygame
